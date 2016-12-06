aa89f67a36b7fc0a5348af64650ce024d3311057
Unknown <hamid5683@gmail.com>
2016-12-06 16:14:55 +0330

    12

---
+++ AndroidVideoChat/app/src/main/java/com/example/hamid/androidvideochat/OnCallDialogListener.java
@@ -0,0 +1,13 @@
+package com.example.hamid.androidvideochat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew Dmitrenko
+ * Date: 7/23/13
+ * Time: 11:05 AM
+ */
+public interface OnCallDialogListener {
+
+    public void onAcceptCallClick();
+    public void onRejectCallClick();
+}
